85a69fcb6d5a2c4f43abcfe9dd44dd99b1061818
damianFresh <67586060+damianStrojek@users.noreply.github.com>
2022-03-23 10:55:27 +0100

    Update ls_recurs.cpp

--- ls_recurs.cpp
+++ ls_recurs.cpp
@@ -9,19 +9,19 @@
 #pragma warning(disable : 4996) // problem z data - znowu
 
 class FInfo {
-    public:
-        bool isFolder;
-        std::string name;
-        __int64 length;
-        std::filesystem::file_time_type modificationDate;
-        FInfo* parent;
-        virtual ~FInfo() {};
+public:
+    bool isFolder;
+    std::string name;
+    __int64 length;
+    std::filesystem::file_time_type modificationDate;
+    FInfo* parent;
+    virtual ~FInfo() {};
 };
 
 class Folder : public FInfo {
-    public:
-        std::list<FInfo*> child;
-        ~Folder();
+public:
+    std::list<FInfo*> child;
+    ~Folder();
 };
 
 Folder::~Folder() {
@@ -31,12 +31,8 @@ Folder::~Folder() {
 int folderSize(Folder* actualFolder) {
     int sum = 0;
     for (std::list<FInfo*>::iterator iter = actualFolder->child.begin(); iter != actualFolder->child.end(); iter++) {
-        if ((*iter)->isFolder == 1) {
-            sum += folderSize((Folder*)*iter);
-        }
-        else {
-            sum += (*iter)->length;
-        }
+        if ((*iter)->isFolder == 1) sum += folderSize((Folder*)*iter);
+        else sum += (*iter)->length;
     }
     return sum;
 };
@@ -74,9 +70,7 @@ Folder* getFiles(std::string dir, Folder* parent = NULL) {
     return parent;
 };
 
-void wciecie(int level) {
-    for (int i = 0; i < level; i++) std::cout << "   ";
-};
+void wciecie(int level) { for (int i = 0; i < level; i++) std::cout << "   "; };
 
 // template dla formatu daty
 template <typename TP> std::time_t to_time_t(TP tp) {
@@ -94,12 +88,8 @@ void writeFiles(Folder* actualFolder, int level) {
         std::cout << "Name: " << child->name << "\n";
 
         wciecie(level);
-        if (child->isFolder) {
-            std::cout << "It is a folder.\n";
-        }
-        else {
-            std::cout << "It is a file.\n";
-        }
+        if (child->isFolder) std::cout << "FOLDER\n";
+        else std::cout << "FILE\n";
 
         wciecie(level);
         std::cout << "Length: " << child->length << "B\n";
@@ -113,40 +103,14 @@ void writeFiles(Folder* actualFolder, int level) {
         std::cout << "Modification date: ";
         std::cout << formattedFileTime << "\n\n";
 
-        if (child->isFolder) {
-            writeFiles((Folder*)child, level + 1);
-        }
+        if (child->isFolder) writeFiles((Folder*)child, level + 1);
     }
-};
-
-// przygotowanie do wywolan rekurencyjnych
-void beforeWrite(Folder* actualFolder) {
-    std::cout << "\nName: " << actualFolder->name << "\n";
-
-    if (actualFolder->isFolder) {
-        std::cout << "It is a folder.\n";
-    }
-    else {
-        std::cout << "It is a file.\n";
-    }
-
-    std::cout << "Length: " << actualFolder->length << "B\n";
-
-    const std::time_t tf = to_time_t(actualFolder->modificationDate);
-    // zamiana formatu czasowego
-    std::tm* gmt = gmtime(&tf);
-    std::stringstream buffer;
-    buffer << std::put_time(gmt, "%A, %d %B %Y %H:%M");
-    std::string formattedFileTime = buffer.str();
-    std::cout << "Modification date: " << formattedFileTime << '\n\n';
-
-    writeFiles(actualFolder, 1);
 };
 
 int main() {
     Folder* actualFolder = new Folder;
     actualFolder = getFiles("D:\\kali");
-    beforeWrite(actualFolder);
+    writeFiles(actualFolder, 1);
 
     delete actualFolder;
     return 0;
